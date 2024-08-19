a03844c628497fa8ec8c147975f1df03c1cbb510
Belal <mohammadvns121@gmail.com>
2024-08-19 18:11:15 +0530

    add some file and play with props

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+</script>
 
 <template>
-  <h1 class="text-2xl">vue job</h1>
+  <Navbar />
+  <Hero title="Test Title" subtitle="Test Subtitle" />
 </template>
 
 <style></style>
