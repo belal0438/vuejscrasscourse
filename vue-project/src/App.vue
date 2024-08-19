8ecfe037b684c9161297d903550b76c0d98d1519
Belal <mohammadvns121@gmail.com>
2024-08-19 17:22:54 +0530

    basic method like add and delete

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,58 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
+
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deletTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+</script>
 
 <template>
-  <h1>Hello from new project</h1>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deletTask(index)">X</button>
+    </li>
+  </ul>
+  <br />
+  <!-- <button v-on:click="toggleStatus">change status</button> -->
+  <button @click="toggleStatus">change status</button>
 </template>
 
 <style scoped></style>
