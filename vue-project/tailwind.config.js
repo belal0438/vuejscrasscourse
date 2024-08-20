f5cd5ef5fbdb6b4248c98dbe24e1e5720d3d4345
Belal <mohammadvns121@gmail.com>
2024-08-20 10:51:39 +0530

    create joblistingView page and add json-server

--- vue-project/tailwind.config.js
+++ vue-project/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -11,10 +11,8 @@ export default {
       },
     },
   },
-
   variants: {
     extend: {},
   },
-
   plugins: [],
 };
